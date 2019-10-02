aa6c177ca3d1fa7ecf3ebef452748519ccd01ef3
Abed1230 <abedfetrat@outlook.com>
2019-10-02 23:26:56 +0200

    fetch chapters from db

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,33 +2,63 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import MyNavBar from './MyNavBar';
 import ListCard from './ListCard';
+import { db } from '../FirebaseData';
 
-var chapters = [{ id: "0", subhead: "Avsnitt 1", title: "Kommunikationens ädla konst", complete: false },
+/* var chapters = [{ id: "0", subhead: "Avsnitt 1", title: "Kommunikationens ädla konst", complete: false },
 { id: "1", subhead: "Avsnitt 2", title: "The Witcher", complete: true },
 { id: "3", subhead: "Avsnitt 3", title: "Minions", complete: true },];
+ */
+class Home extends React.Component {
 
-function Home() {
-    return (
-        <>
-            <MyNavBar />
-            <Container className="mt-3">
-                <Row>
-                    {chapters.map((item, index) => {
-                        return (
-                            /* TODO: navigate to chapter page */
-                            <Col key={item.id} className="mb-2" xs="12" md="6">
-                                <ListCard
-                                    subhead={item.subhead}
-                                    title={item.title}
-                                    complete={item.complete}
-                                    handleCheck={() => console.log(item.title + " checked!")} />
-                            </Col>
-                        );
-                    })}
-                </Row>
-            </Container>
-        </>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            chapters: []
+        };
+    }
+
+    async getData() {
+        /* TODO: get paid chapters if user is paid */
+        const query = await db.collection("free_chapters").orderBy("position").get();
+        const docs = query.docs;
+        const chaps = docs.map((doc) => {
+            const chap = doc.data();
+            chap.id = doc.id;
+            return chap;
+        });
+        
+        this.setState({
+            chapters: chaps
+        });
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    render() {
+        return (
+            <>
+                <MyNavBar />
+                <Container className="mt-3">
+                    <Row>
+                        {this.state.chapters.map((item, index) => {
+                            return (
+                                /* TODO: navigate to chapter page */
+                                <Col key={item.id} className="mb-2" xs="12" md="6">
+                                    <ListCard
+                                        subhead={item.subhead}
+                                        title={item.title}
+                                        complete={item.complete}
+                                        handleCheck={() => console.log(item.title + " checked!")} />
+                                </Col>
+                            );
+                        })}
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 }
 
 export default Home;
