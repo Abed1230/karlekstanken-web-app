ccbf88163e93dfb9c25ff1552a1b276b58cbb1da
Abed1230 <abedfetrat@outlook.com>
2019-11-05 14:15:54 +0100

    add remove partner and delete account modals

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import MyTitleBar from './MyTitleBar';
-import { Row, Col, Container, Card, Dropdown } from 'react-bootstrap';
+import { Spinner, Form, Modal, Row, Col, Container, Card, Dropdown, Button } from 'react-bootstrap';
 import { UserConsumer } from '../UserContext';
 import strftime from 'strftime';
 
 class Settings extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loading: false,
+            showChangePasswordModal: false,
+            showRemovePartnerModal: false,
+            showDeleteAccountModal: false,
+        };
+
+        this.handleDeleteAccount = this.handleDeleteAccount.bind(this);
     }
 
     timeStampToDateString(ts) {
         return strftime('%F %T', ts.toDate());
+    }
+
+    handleRemovePartner() {
+        // TODO: call remove partner cloud function
+    }
+
+    handleDeleteAccount(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        // TODO: call delete account cloud function
+        this.setState({ loading: true });
     }
 
     render() {
@@ -23,8 +42,8 @@ class Settings extends React.Component {
                         return user ?
                             (
                                 <Container>
-                                    <Row className="mt-4 justify-content-center">
-                                        <Col md="6">
+                                    <Row>
+                                        <Col className="mt-4" md="6">
                                             <Card>
                                                 <Card.Body>
                                                     <h5 className="text-center">Inloggad som</h5>
@@ -50,17 +69,82 @@ class Settings extends React.Component {
                                                     <span><strong>Licens</strong></span>
                                                     {user.premium ?
                                                         <p>
-                                                            Betalt {this.timeStampToDateString(user.premium.since)}
+                                                            Sedan {this.timeStampToDateString(user.premium.since)}
                                                             <br />
                                                             <mark>Upphör att gälla {this.timeStampToDateString(user.premium.expiry)}</mark>
                                                         </p>
                                                         :
-                                                        <p>Du har inte köpt license</p>
+                                                        <p>Du har ingen licens</p>
                                                     }
                                                 </Card.Body>
                                             </Card>
                                         </Col>
+                                        <Col className="mt-4" md="6">
+                                            <Button className="mb-4" variant="light" block>Ändra lösenord</Button>
+                                            {!user.premium && user.partner &&
+                                                <>
+                                                    <Button variant="outline-danger" block onClick={() => this.setState({ showRemovePartnerModal: true })}>Ta bort partner</Button>
+                                                    <small className="mt-2 text-muted" /* style={{ fontSize: "0.95rem" }} */>Observera att det inte går att ta bort sin partner när man har licens</small>
+                                                </>
+                                            }
+                                            <Dropdown.Divider />
+                                            <Button className="mt-3 mb-3" variant="danger" onClick={() => this.setState({ showDeleteAccountModal: true })}>Avsluta konto</Button>
+                                            {/* <p className="mt-2 text-muted" style={{ fontSize: "0.95rem" }}>Observera att om du har partner avslutas/tas bort dennes konto även. Om du har köpt license upphör den att gälla</p> */}
+                                        </Col>
                                     </Row>
+
+                                    <Modal show={this.state.showRemovePartnerModal} onHide={() => this.setState({ showRemovePartnerModal: false })}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Är du säker på att du vill ta bort {user.partner && user.partner.name} som partner?</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Footer>
+                                            <Button variant="danger">Ta bort</Button>
+                                            <Button variant="info" onClick={() => this.setState({ showRemovePartnerModal: false })}>Avbryt</Button>
+                                        </Modal.Footer>
+                                    </Modal>
+
+                                    <Modal show={this.state.showDeleteAccountModal} onHide={() => this.setState({ showDeleteAccountModal: false })}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>
+                                                {user.partner && user.premium ?
+                                                    "Är du säker på att du vill avsluta/ta bort ditt konto och " + user.partner.name + "'s konto?"
+                                                    :
+                                                    "Är du säker på att du vill avsluta/ta bort ditt konto?"
+                                                }
+                                            </Modal.Title>
+                                        </Modal.Header>
+                                        {user.partner && user.premium ?
+                                            <Modal.Body>
+                                                <p className="text-danger">Observera att om du avslutar ditt konto avslutas även din partners konto. Er licens upphör att gälla. Detta har omedelbar verkan och kan inte ångras!</p>
+                                                <Form onSubmit={this.handleDeleteAccount}>
+                                                    <Form.Check required type="checkbox" label={"Jag bekräftar att jag vill avsluta mitt och " + user.partner.name + "'s konto"} />
+                                                    <hr />
+                                                    <div className="text-right">
+                                                        {this.state.loading ?
+                                                            <Spinner animation="border" variant="info" />
+                                                            :
+                                                            <>
+                                                                <Button className="mr-2" variant="danger" type="submit">Avsluta</Button>
+                                                                <Button variant="info" onClick={() => this.setState({ showDeleteAccountModal: false })}>Avbryt</Button>
+                                                            </>
+                                                        }
+                                                    </div>
+                                                </Form>
+                                            </Modal.Body>
+                                            :
+                                            <Modal.Footer>
+                                                {this.state.loading ?
+                                                    <Spinner animation="border" variant="info" />
+                                                    :
+                                                    <>
+                                                        <Button className="mr-2" variant="danger" onClick={this.handleDeleteAccount}>Avsluta</Button>
+                                                        <Button variant="info" onClick={() => this.setState({ showDeleteAccountModal: false })}>Avbryt</Button>
+                                                    </>
+                                                }
+                                            </Modal.Footer>
+                                        }
+                                    </Modal>
+
                                 </Container>
                             )
                             :
