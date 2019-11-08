2a96726a0c5ed86dcea4628e37604223cd1f165b
Abed1230 <abedfetrat@outlook.com>
2019-11-09 00:01:19 +0100

    use formik, add confirm password field

--- src/components/Settings/ChangePasswordModal.js
+++ src/components/Settings/ChangePasswordModal.js
@@ -2,111 +2,161 @@ import React from 'react';
 import { Spinner, Form, Modal, Button, Alert } from 'react-bootstrap';
 import { auth, EmailAuthProvider } from '../../FirebaseData';
 import { WRONG_PASSWORD } from '../../AuthErrorCodes';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const ChangePasswordSchema = Yup.object().shape({
+    currentPassword: Yup.string()
+        .min(6, "Lösenordet kan inte vara mindre än 6 tecken")
+        .required("Nuvarande lösenord krävs"),
+    newPassword: Yup.string()
+        .min(6, "Lösenordet måste bestå av minst 6 tecken")
+        .required("Lösenord måste fyllas i"),
+    confirmPassword: Yup.string()
+        .min(6, "Lösenordet måste bestå av minst 6 tecken")
+        .required("Du måste bekräfta nya lösenordet")
+        .oneOf([Yup.ref("newPassword"), null], "Lösenorden stämmer inte överäns"),
+});
 
 class ChangePasswordModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            validated: false,
             loading: false,
             error: null,
             success: false,
         };
 
-        this.hideAndReset = this.hideAndReset.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    hideAndReset() {
+    hideAndReset(resetForm) {
+        if (resetForm) {
+            resetForm();
+        }
         this.props.handleHide();
         setTimeout(() => {
             this.setState({
-                validated: false,
                 error: null,
                 success: false,
+                loading: false,
             });
         }, 500);
     }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        event.stopPropagation();
+    async handleSubmit({ currentPassword, newPassword }, { setErrors }) {
+        this.setState({ loading: true, error: null, success: false });
 
-        const form = event.currentTarget;
+        try {
+            // Re authenticate
+            const user = auth.currentUser;
+            const credential = EmailAuthProvider.credential(user.email, currentPassword);
+            await user.reauthenticateWithCredential(credential);
 
-        if (form.checkValidity() === true) {
-            this.setState({ validated: true, loading: true, error: null, success: false });
+            // Update password
+            await user.updatePassword(newPassword);
 
-            const currPassword = form.elements.currPassword.value;
-            const newPassword = form.elements.newPassword.value;
+            this.setState({ loading: false, success: true });
+        } catch (e) {
+            console.log(e);
+            if (e.code === WRONG_PASSWORD) {
+                setErrors({ currentPassword: " " });
+                this.setState({
+                    loading: false,
+                    error: "Du har angivit fel nuvarande lösenord. Försök igen"
+                });
 
-            try {
-                // Re authenticate
-                const user = auth.currentUser;
-                const credential = EmailAuthProvider.credential(user.email, currPassword);
-                await user.reauthenticateWithCredential(credential);
-
-                // Update password
-                await user.updatePassword(newPassword);
-
-                this.setState({ loading: false, success: true });
-            } catch (e) {
-                console.log(e);
-                switch (e.code) {
-                    case WRONG_PASSWORD:
-                        this.setState({ loading: false, error: "Du har angivit fel nuvarande lösenord. Försök igen", validated: false });
-                        break;
-                    default:
-                        this.setState({ loading: false, error: "Ett okänt fel inträffade. Försök igen", validated: false });
-                        break;
-                }
+            } else {
+                this.setState({
+                    loading: false,
+                    error: "Ett okänt fel inträffade. Försök igen"
+                });
             }
         }
     }
 
     render() {
         return (
-            <Modal show={this.props.show} onHide={this.hideAndReset}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Ändra lösenord</Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="p-0">
-                    <Form validated={this.state.validated} onSubmit={this.handleSubmit}>
-                        <div className="p-2">
+            <Formik
+                initialValues={{
+                    currentPassword: "",
+                    newPassword: "",
+                    confirmPassword: "",
+                }}
+                validationSchema={ChangePasswordSchema}
+                onSubmit={this.handleSubmit}>
+                {({ handleSubmit, handleChange, values, errors, touched, resetForm }) => (
+                    <Modal show={this.props.show} onHide={this.hideAndReset.bind(this, resetForm)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Ändra lösenord</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
                             {this.state.success ?
                                 <Alert variant="success">Ditt lösenord har nu ändrats</Alert>
                                 :
-                                <>
-                                    <Form.Group controlId="currPasswordField">
+                                <Form noValidate>
+                                    <Form.Group controlId="currentPasswordField">
                                         <Form.Label>Ditt nuvarande lösenord</Form.Label>
-                                        <Form.Control required type="password" name="currPassword" placeholder="Nuvarande lösenord" pattern=".{6,}" title="Lösenordet måste bestå av minst 6 tecken" />
+                                        <Form.Control
+                                            type="password"
+                                            name="currentPassword"
+                                            placeholder="Nuvarande lösenord"
+                                            value={values.currentPassword}
+                                            onChange={handleChange}
+                                            isInvalid={touched.currentPassword && !!errors.currentPassword} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.currentPassword}
+                                        </Form.Control.Feedback>
                                     </Form.Group>
                                     <Form.Group controlId="newPassWordField">
                                         <Form.Label>Nytt lösenord</Form.Label>
-                                        <Form.Control required type="password" name="newPassword" placeholder="Nytt lösenord" pattern=".{6,}" title="Lösenordet måste bestå av minst 6 tecken" />
+                                        <Form.Control
+                                            type="password"
+                                            name="newPassword"
+                                            placeholder="Nytt lösenord (minst 6 tecken)"
+                                            value={values.newPassword}
+                                            onChange={handleChange}
+                                            isInvalid={touched.newPassword && !!errors.newPassword} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.newPassword}
+                                        </Form.Control.Feedback>
                                     </Form.Group>
-                                    {this.state.error &&
-                                        <Alert variant="danger">{this.state.error}</Alert>
-                                    }
-                                </>
+                                    <Form.Group controlId="confirmPassWordField">
+                                        <Form.Label>Bekräfta nytt lösenord</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            name="confirmPassword"
+                                            placeholder="Bekräfta nytt lösenord"
+                                            value={values.confirmPassword}
+                                            onChange={handleChange}
+                                            isInvalid={touched.confirmPassword && !!errors.confirmPassword} />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.confirmPassword}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Form>
                             }
-                        </div>
+                            {this.state.error &&
+                                <Alert variant="danger">{this.state.error}</Alert>
+                            }
+                        </Modal.Body>
                         <Modal.Footer>
                             {this.state.loading ?
                                 <Spinner animation="border" variant="info" />
                                 :
                                 this.state.success ?
-                                    <Button variant="info" onClick={this.hideAndReset}>Stäng</Button>
+                                    <Button variant="info" onClick={this.hideAndReset.bind(this, resetForm)}>Stäng</Button>
                                     :
                                     <>
-                                        <Button variant="light" onClick={this.hideAndReset}>Avbryt</Button>
-                                        <Button variant="info" type="submit">Spara</Button>
+                                        <Button variant="light" onClick={this.hideAndReset.bind(this, resetForm)}>Avbryt</Button>
+                                        <Button variant="info" onClick={handleSubmit}>Spara</Button>
                                     </>
                             }
                         </Modal.Footer>
-                    </Form>
-                </Modal.Body>
-            </Modal>
+                    </Modal>
+                )
+                }
+            </Formik>
         );
     }
 }
