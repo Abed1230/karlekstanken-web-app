c6b5d0d985af04b270cb22b0b0031034bdae050b
Abed1230 <abedfetrat@outlook.com>
2019-11-17 15:08:29 +0100

    add print button

--- src/components/Task/TaskPage.js
+++ src/components/Task/TaskPage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Redirect } from 'react-router-dom';
 import { db } from '../../FirebaseData';
 import MyNavBar from '../MyNavBar';
 import time from '../../assets/time.svg';
 import './TaskPage.css';
 import PrintTask from './PrintTask';
+
+const PrintIcon = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill={props.fill} width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M19 8h-1V3H6v5H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zM8 5h8v3H8V5zm8 12v2H8v-4h8v2zm2-2v-2H6v2H4v-4c0-.55.45-1 1-1h14c.55 0 1 .45 1 1v4h-2z" /><circle cx="18" cy="11.5" r="1" /></svg>
+);
 
 export class TaskPage extends Component {
     constructor(props) {
@@ -59,6 +63,13 @@ export class TaskPage extends Component {
                     <>
                         <Container className="d-print-none">
                             <Row className="mt-4">
+                                <Col>
+                                    <button onClick={() => window.print()}
+                                        style={{ backgroundColor: "transparent", border: "0px" }}>
+                                        <span className="mr-1"><PrintIcon fill="#000" /></span>
+                                        Skriv ut
+                                    </button>
+                                </Col>
                                 <Col className="text-right" /* className="d-flex justify-content-end" */>
                                     <div className="d-inline-block text-center">
                                         <img
@@ -71,7 +82,7 @@ export class TaskPage extends Component {
                                     </div>
                                 </Col>
                             </Row>
-                            <Row className="mt-4 justify-content-center">
+                            <Row className="mb-4 justify-content-center">
                                 <Col md="9">
                                     <div dangerouslySetInnerHTML={{ __html: task.bodyHTML }} />
                                 </Col>
