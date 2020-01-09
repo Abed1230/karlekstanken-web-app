65563e008e13a76b93faa589ed0bbe55a8623151
Abed1230 <abedfetrat@outlook.com>
2020-01-09 21:29:08 +0100

    minor changes

--- src/components/InstallationGuide/InstallBanner.js
+++ src/components/InstallationGuide/InstallBanner.js
@@ -4,11 +4,13 @@ import TransparentButton from '../TransparentButton';
 import { CloseIcon } from '../../assets/svgs';
 
 const InstallBanner = ({ handleClick, handleClose }) => (
-    <div className="bg-dark text-center text-white fixed-bottom pt-3 pb-3 pl-4 pr-4">
+    <div className="bg-dark text-center text-white fixed-bottom pt-3 pb-3 pl-4 pr-4" style={{ zIndex: 1 }}>
         <Row className="d-flex align-items-center ">
             <Col>
-                <h6>Lägg till Kärlekstanken på din hemskärm.</h6>
-                <Button size="sm" variant="outline-info" onClick={handleClick}>Se hur du gör</Button>
+                <span>Lägg till Kärlekstanken på din hemskärm.</span>
+                <br />
+                {/* <Button size="sm" variant="outline-info" onClick={handleClick}>Se hur du gör</Button> */}
+                <TransparentButton onClick={handleClick} variant="outline-info">Tryck här</TransparentButton>
             </Col>
             <Col className="" xs="2" lg="1">
                 <TransparentButton onClick={handleClose}>
