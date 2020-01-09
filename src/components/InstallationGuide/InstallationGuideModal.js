59188e5da345e9618b598fa3c9f39d47d12404eb
Abed1230 <abedfetrat@outlook.com>
2020-01-09 14:55:24 +0100

    add android installation instructions

--- src/components/InstallationGuide/InstallationGuideModal.js
+++ src/components/InstallationGuide/InstallationGuideModal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import SafariIcon from '../../assets/safari.svg';
 import IosShareIcon from '../../assets/ios_share_icon.svg';
+import ChromeIcon from '../../assets/chrome.svg';
+import ChromeMoreIcon from '../../assets/more_vert-24px.svg';
 
 const isIos = () => {
     const userAgent = window.navigator.userAgent.toLowerCase();
@@ -39,7 +41,7 @@ const IOSInstructions = () => (
     <>
         <span>Du kan installera Kärlekstanken på din hemskärm med webbläsaren </span>
         <div className="d-inline-block bg-light p-1 rounded">
-            <img src={SafariIcon} width={32} height={32} className="mr-1" />
+            <img src={SafariIcon} width={24} height={24} className="mr-1" />
             Safari
         </div>
         <div className="mt-3">
@@ -47,13 +49,31 @@ const IOSInstructions = () => (
             <ol>
                 <li>Tryck på <img src={IosShareIcon} width={32} height={32} /> (dela) som finns i webbläsarens botten eller topp</li>
                 <li>Välj "Lägg till på hemskärmen"</li>
+                <li>Lägg till</li>
             </ol>
             <p className="text-muted">Notera: Du kommer att behöva logga in på nytt när du startar appen från hemskärmen för första gången.</p>
         </div>
     </>
 );
 
-const AndroidInstructions = () => ("is Android");
+const AndroidInstructions = () => (
+    <>
+        <span>Du kan installera Kärlekstanken på din hemskärm med webbläsaren </span>
+        <div className="d-inline-block bg-light p-1 rounded">
+            <img src={ChromeIcon} width={24} height={24} className="mr-1" />
+            Google Chrome
+        </div>
+        <div className="mt-3">
+            <p>Gör så här:</p>
+            <ol>
+                <li>Tryck på <img src={ChromeMoreIcon} width={24} height={24} /> som finns i webbläsarens topp</li>
+                <li>Välj "Lägg till på startskärmen"</li>
+                <li>Lägg till</li>
+            </ol>
+            <p className="text-muted">Notera: Du kommer att behöva logga in på nytt när du startar appen från hemskärmen för första gången.</p>
+        </div>
+    </>
+);
 
 const OtherInstructions = () => ("is windows or mac");
 
