90fe54877c05740948e71d24751961c70142851a
Abed1230 <abedfetrat@outlook.com>
2020-01-09 22:53:28 +0100

    add support mail to side menu

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -8,10 +8,7 @@ import { UserConsumer } from '../UserContext';
 import Sidebar from "react-sidebar";
 import './MyNavBar.css';
 import { CloseIcon, BackIcon } from '../assets/svgs';
-
-const GearIcon = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill={props.fill} width="22" height="22" viewBox="0 0 24 24"><path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" /></svg>
-);
+import MyStrings from '../MyStrings.json';
 
 const mql = window.matchMedia(`(max-width: 350px)`);
 
@@ -87,6 +84,14 @@ class MyNavBar extends React.Component {
                                     <Button block variant="info" as={Link} to="/signup">SKAPA KONTO</Button>
                                 </div>
                             }
+                            <div style={{ marginTop: "50px" }}>
+                                <hr />
+                                {/* <a href="#" style={{fontSize: "1.2rem"}}>Om kärlekstanken</a> */}
+                                <p>Eventuella frågor är du välkommen att ställa till <a href="mailto:kundservice@karlekstanken.se">kundservice@karlekstanken.se</a></p>
+                                <a href={MyStrings.licenseTermsUrl} target="_blank">Användarvilkor</a>
+                                <br />
+                                <a href={MyStrings.privacyPolicyUrl} target="_blank">Personuppgiftspolicy</a>
+                            </div>
                         </div>
                     }
                     open={this.state.sidebarOpen}
@@ -97,7 +102,8 @@ class MyNavBar extends React.Component {
                             position: "fixed",
                             width: mql.matches ? [`${window.innerWidth - 50}px`] : "300px",
                             background: "linear-gradient(to right, #ffafbd, #ffc3a0)",
-                            zIndex: 3, paddingTop: "70px"
+                            zIndex: 3,
+                            paddingTop: "70px",
                         },
                         overlay: { zIndex: 2 }
                     }}
