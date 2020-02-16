fd31aec29f6e98aa3ca79fbc4cd76c894ce9efe8
Abed1230 <abedfetrat@outlook.com>
2020-02-16 12:46:08 +0100

    change add partner modal layout

---
+++ src/MyStrings.js
@@ -0,0 +1,25 @@
+const MyStrings = {
+    add: "Lägg till",
+    yourEmail: "Din e-postadress",
+    email: "E-postadress",
+    AddPartnerModal: {
+        title: "Lägg till partner",
+        title2: "Bjud in partner",
+        successText: "har nu lagts till som din partner",
+        closeBtn: "Stäng",
+        undeterminedViewText: "Har din partner ett konto på Kärlekstanken?",
+        undeterminedViewYesBtn: "Ja, lägg till",
+        undeterminedViewNoBtn: "Nej, bjud in",
+        addFormViewLabel: "Ange din partners e-postadress som hen registrerade sig med på Kärlekstanken",
+        addFormViewAddBtn: "Lägg till",
+        inviteViewText1: "Bjud in din partner till Kärlekstanken. Hen blir tillagd som din partner i Kärlekstanken när hen har registrerat sig genom länken som skickas med knappen nedan.",
+        inviteViewText2: "Beroende på din enhet öppnar knappen ovan standardapplikationen för e-postmeddelanden eller deladialogen där du kan välja den app du vill skicka inbjudan med. Du behöver själv välja mottagaren och trycka på skicka.",
+        inviteViewInviteBtn: "Bjud in",
+    },
+    Errors: {
+        invalidEmail: "Ogiltig e-postadress",
+        fieldRequired: "Fältet kan inte vara tomt"
+    }
+};
+
+export default MyStrings;
--- src/components/AddPartnerModal.js
+++ src/components/AddPartnerModal.js
@@ -3,26 +3,84 @@ import { Modal, Spinner, Form, Alert, Button } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { addPartner } from '../MyCloudFunctions';
-import MyStrings from '../MyStrings.json';
-import TransparentButton from "./TransparentButton";
 import { StringsConsumer } from '../contexts/StringsContext';
+import MyStrings from '../MyStrings.js';
 
 const EmailSchema = Yup.object().shape({
     email: Yup.string()
-        .email(MyStrings.errors.invalidEmail)
-        .required(MyStrings.errors.fieldRequired)
+        .email(MyStrings.Errors.invalidEmail)
+        .required(MyStrings.Errors.fieldRequired)
 });
+
+function Undetermined({ handleYes, handleNo }) {
+    return (
+        <>
+            <h6>{MyStrings.AddPartnerModal.undeterminedViewText}</h6>
+            <Button block variant="info" onClick={(handleYes)}>{MyStrings.AddPartnerModal.undeterminedViewYesBtn}</Button>
+            <Button block variant="info" onClick={handleNo}>{MyStrings.AddPartnerModal.undeterminedViewNoBtn}</Button>
+        </>
+    );
+}
+
+function AddForm({ handleSubmit, handleChange, values, errors, touched, error, loading, userEmail }) {
+    return (
+        <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="emailField">
+                <Form.Label>{MyStrings.AddPartnerModal.addFormViewLabel}</Form.Label>
+                <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder={MyStrings.email}
+                    value={values.email}
+                    onChange={handleChange}
+                    isInvalid={touched.email && !!errors.email} />
+                <Form.Control.Feedback type="invalid">
+                    {errors.email}
+                </Form.Control.Feedback>
+                <p className="text-muted mt-2" style={{ fontSize: "0.95rem" }}>
+                    {MyStrings.yourEmail}: {userEmail}
+                </p>
+
+            </Form.Group>
+            {error &&
+                <Alert variant="danger">{error}</Alert>
+            }
+            {loading ?
+                <Spinner animation="border" variant="info" />
+                :
+                <Button block variant="info" type="submit">{MyStrings.AddPartnerModal.addFormViewAddBtn}</Button>
+            }
+        </Form>
+    );
+}
+
+function Invite({ handleClick }) {
+    return (
+        <>
+            <p>{MyStrings.AddPartnerModal.inviteViewText1}</p>
+            <Button className="mb-2" block variant="info" onClick={handleClick}>{MyStrings.AddPartnerModal.inviteViewInviteBtn}</Button>
+            <p className="text-muted" style={{ fontSize: "0.9rem" }}>{MyStrings.AddPartnerModal.inviteViewText2}</p>
+        </>
+    );
+}
+
+const UNDETERMINED_VIEW = "Undetermined";
+const ADD_FORM_VIEW = "AddForm";
+const INVITE_VIEW = "Invite"
 
 class AddPartnerModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            toRender: UNDETERMINED_VIEW,
             loading: false,
             error: null,
             success: false,
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleYes = this.handleYes.bind(this);
+        this.handleNo = this.handleNo.bind(this);
     }
 
     hideAndReset(resetForm) {
@@ -32,6 +90,7 @@ class AddPartnerModal extends React.Component {
         this.props.handleHide();
         setTimeout(() => {
             this.setState({
+                toRender: UNDETERMINED_VIEW,
                 error: null,
                 loading: false,
                 success: false,
@@ -51,21 +110,39 @@ class AddPartnerModal extends React.Component {
         }
     }
 
-    handleShare(strings) {
-        const invitationTitle = strings && strings.invitationTitle;
-        const invitationText = strings && strings.invitationText;
-        const invitationMailText = strings && strings.invitationMailText;
+    handleShare(strings, userFullName) {
+        let invitationTitle = strings && strings.invitationTitle;
+        if (invitationTitle)
+            invitationTitle = invitationTitle.replace(/XXXX/g, userFullName);
+
+        let invitationText = strings && strings.invitationText;
+        if (invitationText) {
+            invitationText = invitationText.replace(/XXXX/g, userFullName);
+            invitationText = invitationText.replace(/\\n/g, '\n');
+        }
 
         if (window.navigator.share) {
             window.navigator.share({
                 title: invitationTitle,
-                text: invitationText.replace(/\\n/g, '\n'),
+                text: invitationText,
             });
         } else {
             const mail = document.createElement("a");
-            mail.href = "mailto:?subject=" + invitationTitle + "&body=" + invitationMailText;
+            mail.href = "mailto:?subject=" + invitationTitle + "&body=" + encodeURIComponent(invitationText);
             mail.click();
         }
+    }
+
+    handleYes() {
+        this.setState({
+            toRender: ADD_FORM_VIEW
+        });
+    }
+
+    handleNo() {
+        this.setState({
+            toRender: INVITE_VIEW
+        });
     }
 
     render() {
@@ -79,54 +156,44 @@ class AddPartnerModal extends React.Component {
                 {({ handleSubmit, handleChange, values, errors, touched, resetForm }) => (
                     <Modal show={this.props.show} onHide={this.hideAndReset.bind(this, resetForm)}>
                         <Modal.Header closeButton>
-                            <Modal.Title>Lägg till partner</Modal.Title>
+                            <Modal.Title>{this.state.toRender === INVITE_VIEW ? MyStrings.AddPartnerModal.title2 : MyStrings.AddPartnerModal.title}</Modal.Title>
                         </Modal.Header>
-                        <Modal.Body>
+                        <Modal.Body className="pb-4">
                             {this.state.success ?
                                 <>
                                     <Alert variant="success">
-                                        Partnerförfrågan skickades. Din partner hittar förfågan i menyn.
+                                        {this.props.user.partner && this.props.user.partner.name} {MyStrings.AddPartnerModal.successText}
                                     </Alert>
-                                    <Button variant="info" onClick={this.hideAndReset.bind(this, resetForm)}>Stäng</Button>
+                                    <Button variant="info" onClick={this.hideAndReset.bind(this, resetForm)}>{MyStrings.AddPartnerModal.closeBtn}</Button>
                                 </>
                                 :
-                                <Form noValidate onSubmit={handleSubmit}>
-                                    <Form.Group controlId="emailField">
-                                        <Form.Label>
-                                            Ange din partners e-postadress som hen registrerade sig med på Kärlekstanken.
-                                            <br />
-                                            Har din partner inte har registrerat sig ännu?
-                                            <br />
-                                            <StringsConsumer>
-                                                {strings => (
-                                                    <TransparentButton className="text-primary" onClick={() => this.handleShare(strings)}>Skicka inbjudan här</TransparentButton>
-                                                )}
-                                            </StringsConsumer>
-                                        </Form.Label>
-                                        <Form.Control
-                                            type="email"
-                                            name="email"
-                                            placeholder="E-postadress"
-                                            value={values.email}
-                                            onChange={handleChange}
-                                            isInvalid={touched.email && !!errors.email} />
-                                        <Form.Control.Feedback type="invalid">
-                                            {errors.email}
-                                        </Form.Control.Feedback>
-                                        <p className="text-muted mt-2" style={{ fontSize: "0.95rem" }}>
-                                            Din e-postadress: {this.props.user.email}
-                                        </p>
-
-                                    </Form.Group>
-                                    {this.state.error &&
-                                        <Alert variant="danger">{this.state.error}</Alert>
+                                <>
+                                    {this.state.toRender === UNDETERMINED_VIEW &&
+                                        <Undetermined handleYes={this.handleYes} handleNo={this.handleNo} />
                                     }
-                                    {this.state.loading ?
-                                        <Spinner animation="border" variant="info" />
-                                        :
-                                        <Button variant="info" type="submit">Skicka förfrågan</Button>
+                                    {this.state.toRender === ADD_FORM_VIEW &&
+                                        <AddForm
+                                            handleSubmit={handleSubmit}
+                                            handleChange={handleChange}
+                                            values={values}
+                                            errors={errors}
+                                            touched={touched}
+                                            error={this.state.error}
+                                            loading={this.state.loading}
+                                            userEmail={this.props.user.email}
+                                        />
                                     }
-                                </Form>
+                                    {this.state.toRender === INVITE_VIEW &&
+                                        <StringsConsumer>
+                                            {strings => {
+                                                const userFullName = this.props.user.firstName + " " + this.props.user.lastName;
+                                                return (
+                                                    <Invite handleClick={() => this.handleShare(strings, userFullName)} />
+                                                );
+                                            }}
+                                        </StringsConsumer>
+                                    }
+                                </>
                             }
                         </Modal.Body>
                     </Modal>
